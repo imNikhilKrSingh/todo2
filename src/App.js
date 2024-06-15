bbc3873a3fcaeabc81be48ea8759237d48ecf451
imNikhilKrSingh <nikhil1651998@gmail.com>
2024-06-15 13:43:35 +0530

    Todo completed by adding edit and update functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { useState } from 'react';
 import { AiFillDelete } from "react-icons/ai";
+import { RiEditFill } from "react-icons/ri";
+
 
 
 function App() {
 
 const [input, setInput] = useState("");
 const [list, setList] = useState([]);
+const [taskIndex,setTaskIndex] = useState();
+const [update, setUpdate] = useState(false);
 
 const handleInput = (e)=>{
   setInput(e.target.value);
@@ -17,25 +21,43 @@ const handleTask = ()=>{
   setInput("");
 }
 
+const handleUpdate = ()=>{
+  list.splice(taskIndex, 1, input);
+  setInput("");
+  setUpdate(false);
+}
+
 const handleDelete = (index)=>{
   const filterList = list.filter((elem)=> elem !== list[index])
   setList(filterList);
   
 }
 
+const handleEdit = (index)=>{
+  const filterList = list.filter((elem)=> elem === list[index])
+  setInput(filterList[0]);
+  setTaskIndex(index);
+  setUpdate(true);
+  }
+
 
   return (
     <div className="App">
       <h1> Todo App </h1>
+
       <div className="container">
         <div className="input-box">
-          <input type="text" value={input} onChange={(e)=>handleInput(e)}/> <button onClick={handleTask}> Add Task </button>
+          <input type="text" value={input} onChange={(e)=>handleInput(e)} placeholder="Enter your task..."/>
+          { update ? <button onClick={handleUpdate}> Update Task </button> :
+            <button onClick={handleTask}> Add Task </button> }
         </div>
+
+
         <div className="list">
           <ul>
-            {list.map((item, index)=><li key={index}>{item} 
-              
-            <AiFillDelete className="deleteIcon" onClick={()=>handleDelete(index)}/> </li>)}
+            {list.map((item, index)=><li key={index}>{item}               
+            <AiFillDelete className="deleteIcon" onClick={()=>handleDelete(index)}/>
+            <RiEditFill className="editIcon" onClick={()=>handleEdit(index)}/> </li>)}
           </ul>
         </div>
       </div>
